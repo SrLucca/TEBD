5a8c2ae96ff651fad810cbc6ae434707e413166d
Lucca Soares <lucca.soares@targetinformacoes.com.br>
2024-05-23 16:41:50 -0300

    Tela inicial do jogo

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
 from tkinter import messagebox
+import tkinter as tk
+import time
+from handler.exit import sair
 
 
-def jogar():
-    messagebox.showinfo("Jogar", "Iniciando o jogo...")
+def jogar(root):
+
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    # Desenha a nova tela do jogo
+    lbl_jogo = tk.Label(root, text="Tela do Jogo", font=("Helvetica", 16))
+    lbl_jogo.pack(pady=20)
+
+    btn_sair = tk.Button(root, text="Sair", command=lambda: sair(root))
+    btn_sair.pack(pady=10)
