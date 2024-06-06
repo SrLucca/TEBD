c8f1769a2207fb801144246ede05851279ebde37
Lucca Soares <lucca.soares@targetinformacoes.com.br>
2024-06-06 16:50:19 -0300

    Adicao de turnos

--- game.py
+++ game.py
@@ -2,8 +2,20 @@ import tkinter as tk
 from handler.exit import sair
 from tkinter import messagebox
 
-def test_func(carta):
-    messagebox.showinfo(f"Carta: {carta}")
+TURNO = 0
+
+def test_func(carta, turno_label):
+    global TURNO  # Referenciando a variável global
+    messagebox.showinfo("Carta", f"Carta: {carta}")
+    
+    # Trocar turno
+    if TURNO == 1:
+        TURNO = 0
+    else:
+        TURNO = 1
+
+    # Atualizar o label de turno
+    turno_label.config(text=f"Turno: {TURNO}")
 
 def jogar(root):
     for widget in root.winfo_children():
@@ -21,18 +33,21 @@ def jogar(root):
 
     
     cartas = [
-        "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠",
-        "J♠", "Q♠", "K♠", "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥"
+        "A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7"
     ]
+
+    turno_label = tk.Label(root, text=f"Turno: {TURNO}")
+    turno_label.pack()
 
     # Adiciona 10 botões na metade superior
     for i in range(10):
-        btn_carta = tk.Button(frame_superior, text=cartas[i], width=10, height=5, command=lambda c=cartas[i]: test_func(c))
+        btn_carta = tk.Button(frame_superior, text=cartas[i], width=10, height=5, command=lambda c=cartas[i]: test_func(c, turno_label))
         btn_carta.grid(row=i//10, column=i%10, padx=10, pady=10)
 
     # Adiciona 10 botões na metade inferior
     for i in range(10, 20):
-        btn_carta = tk.Button(frame_inferior, text=cartas[i], width=10, height=5, command=lambda c=cartas[i]: test_func(c))
+        btn_carta = tk.Button(frame_inferior, text=cartas[i], width=10, height=5, command=lambda c=cartas[i]: test_func(c, turno_label))
         btn_carta.grid(row=(i-10)//10, column=(i-10)%10, padx=10, pady=150)
     
     btn_monte = tk.Button(root, text="Monte", command=lambda: print("Monte clicado"))
